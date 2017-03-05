5241c2fda2a885b272a545c16ee5bed9c8568556
Chad Brewbaker <crb002@gmail.com>
2017-03-05 14:23:35 -0600

    diff of directory changes

--- buildLambdaClang.sh
+++ buildLambdaClang.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 echo Build started on `date`
-yum install -v -y clang
-clang -v
+ls /usr/bin > usrbindir
+ls /lib > libdir
+ls /lib64 > lib64dir
+ls /usr/lib > usrlibdir
+yum install -y clang
+
+ls /usr/bin > usrbindir2
+ls /lib > libdir2
+ls /lib64 > lib64dir2
+ls /usr/lib > usrlibdir2
+diff usrbindir usrbindr2
+diff libdir libdir2
+diff lib64dir lib64dir2
+diff usrlibdir usrlibdir2
+
+
+#clang -v
+
 #ldd /usr/bin/clang
