7cac949799a0a464e6af9a531677576d1a7db24e
Chad Brewbaker <crb002@gmail.com>
2017-03-05 14:38:41 -0600

    update

--- buildLambdaClang.sh
+++ buildLambdaClang.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 echo Build started on `date`
-ls /usr/bin > usrbindir
-ls /lib > libdir
-ls /lib64 > lib64dir
-ls /usr/lib > usrlibdir
+ls /usr/bin | sort > usrbindir
+ls /lib | sort > libdir
+ls /lib64 | sort> lib64dir
+ls /usr/lib | sort > usrlibdir
 yum install -y clang
 
-ls /usr/bin > usrbindir2
-ls /lib > libdir2
-ls /lib64 > lib64dir2
-ls /usr/lib > usrlibdir2
-diff usrbindir usrbindr2
-diff libdir libdir2
-diff lib64dir lib64dir2
-diff usrlibdir usrlibdir2
+ls /usr/bin | sort > usrbindir2
+ls /lib | sort  > libdir2
+ls /lib64 | sort > lib64dir2
+ls /usr/lib | sort > usrlibdir2
+touch 
+diff usrbindir usrbindir2 > usrbin.dif
+diff libdir libdir2 > lib.dif
+diff lib64dir lib64dir2 > lib64.dif
+diff usrlibdir usrlibdir2 > usrlib.dif
 
+mkdir clangOnLambda
+mv *.dif ./clangOnLambda/
+
+tar -c clangOnLambda > clangOnLambda.tar
 
 #clang -v
 
